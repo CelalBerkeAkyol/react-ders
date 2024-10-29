180684fe859653f6d2f102274fd2cd8021a96a8b
CelalBerkeAkyol <buscberke@gmail.com>
2024-10-29 12:43:29 +0300

    state ders

---
+++ steps/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const messages = ["learn React", "Apply for jobs", "Invest Your new income"];
+
+function App() {
+  const [step, setStep] = useState(1);
+
+  function handlePrevius() {
+    if (step > 1) setStep(step - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">
+        step {step} : {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevius}
+        >
+          previus
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+export default App;
